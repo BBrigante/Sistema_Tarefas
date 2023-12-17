37655b8e96ec50cb1ce40cb9230011b779e94dc2
Bruno Brigante <111623017+BBrigante@users.noreply.github.com>
2023-12-17 19:34:39 -0300

    Upload Sistema de Tarefas

---
+++ Enums/StatusTarefas.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace SistemaDeTarefas.Enums
+{
+    public enum StatusTarefas
+    {
+        [Description("A Fazer")]
+        aFazer = 1,
+        [Description("Em Andamento")]
+        EmAndamento = 2,
+        [Description("Concluído")]
+        Concluido = 3
+
+    }
+}
